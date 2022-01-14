148a53d49c7125925ff1ebf21e483962826639f1
bberos <bberos.171@gmail.com>
2022-01-14 18:41:20 -0300

    styles(CharacterCard): character cards styles

---
+++ src/components/CharacterCard/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: grey;
+  width: 15vw;
+  height: 80vh;
+  align-items: center;
+  border-radius: 15px;
+  margin: 10px 10px;
+  h1 {
+    font-size: 1.5rem;
+    width: 100%;
+  }
+  .characterImage {
+    width: 10vw;
+    width: 100%;
+  }
+`;
