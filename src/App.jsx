f1a05050b5ad02f3432d8cd7201e1e052053f0c1
bberos <bberos.171@gmail.com>
2022-01-14 18:40:00 -0300

    feat(App): Header & Routes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import MyRoutes from "./MyRoutes";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MyRoutes />
+    </div>
+  );
+}
+
+export default App;
