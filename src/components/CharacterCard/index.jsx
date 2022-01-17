80e796661d9838d946b8d1bc5fcd633ef5669e79
bberos <bberos.171@gmail.com>
2022-01-17 19:08:59 -0300

    ref(CharacterdCard): simplify info

--- src/components/CharacterCard/index.jsx
+++ src/components/CharacterCard/index.jsx
@@ -1,16 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Container } from "./styles";
 function CharacterCard({ characterData }) {
-  const { name, image, status, species, origin, location } = characterData;
+  const { id, name, image } = characterData;
   return (
-    <Container>
-      <h1>{name}</h1>
-      <img className="characterImage" src={image} alt="character-image" />
-      <h2>Status: {status}</h2>
-      <h2>Specie: {species}</h2>
-      <h2>Origin: {origin.name}</h2>
-      <h2>Location: {location.name}</h2>
-    </Container>
+    <Link to={"/character/" + id}>
+      <Container>
+        <h1>{name}</h1>
+        <img className="characterImage" src={image} alt="character-image" />
+      </Container>
+    </Link>
   );
 }
 
