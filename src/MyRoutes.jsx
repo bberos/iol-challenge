e7a1ab51878669cd152f9e996ec5e8760df7de59
bberos <bberos.171@gmail.com>
2022-01-17 19:08:31 -0300

    feat(Routes): new dynamic route for character detail screen

--- src/MyRoutes.jsx
+++ src/MyRoutes.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import CharacterDetail from "./screens/CharacterDetail";
 import Home from "./screens/Home";
 function MyRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path={"/character/:id"} element={<CharacterDetail />} />
     </Routes>
   );
 }
