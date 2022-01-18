e4c8a0594ce136c0feb876fdc5246ae14eab4b4c
bberos <bberos.171@gmail.com>
2022-01-18 01:46:34 -0300

    feat(Home): filter by location.name logic

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -10,14 +10,22 @@ import ReactPaginate from "react-paginate";
 import ReactSelect from "react-select";
 
 export default function Home() {
-  const [totalCharacters, setTotalCharacters] = useState();
+  const [totalCharacters, setTotalCharacters] = useState(0);
   const [locations, setLocations] = useState();
-  const [selectedLocation, setSelectedLocation] = useState("");
+  const [selectedLocation, setSelectedLocation] = useState();
   const [options, setOptions] = useState([]);
   const [actualPageIds, setActualPageIds] = useState("");
+
   const [characters, setCharacters] = useState([]);
+  const [idsByLocation, setIdsByLocation] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [page, setPage] = useState(1);
+  // const [pages, setPages] = useState({});
+  // ACA ARMO MY ARRAY DE .leng = 826 ITEMS
+  let myArray = [];
+  const limit = 15;
+  let totalPaginas = Math.ceil(totalCharacters / limit);
+  const pages = {};
   useEffect(() => {
     fetchNumberOfCharacters();
     fetchLocations();
@@ -25,17 +33,25 @@ export default function Home() {
   useEffect(() => {
     if (totalCharacters !== undefined) {
       hacerArray();
-      hacerObjeto();
+      // hacerObjeto();
       dividirArray();
-      pageData();
+      if (selectedLocation === undefined) {
+        pageData();
+      }
     }
   }, [totalCharacters, page]);
 
   useEffect(() => {
-    if (actualPageIds !== "") {
-      fetchCharacters();
+    fetchCharacters();
+  }, [actualPageIds]);
+
+  useEffect(() => {
+    if (selectedLocation !== undefined) {
+      cortarIds();
+      pageDataByLocation();
     }
-  }, [actualPageIds, page]);
+  }, [selectedLocation, page]);
+
   const fetchNumberOfCharacters = async () => {
     const charactersData = await getAllCharactersList();
     setTotalCharacters(charactersData.info.count);
@@ -57,18 +73,11 @@ export default function Home() {
     setOptions(options);
   };
 
-  // ACA ARMO MY ARRAY DE .leng = 826 ITEMS
-  let myArray = [];
-  const limit = 15;
-  let totalPaginas = Math.ceil(totalCharacters / limit);
-  let pages = {};
-
   // CREAMOS EL ARRAY DE TODOS LOS IDS
   const hacerArray = () => {
     for (let index = 1; index <= totalCharacters; index++) {
       myArray.push(index);
     }
-    console.log("EL HACER ARRAY");
   };
 
   // CREAMOS EL OBJETO PAGINAS CON LA KEY
@@ -77,13 +86,12 @@ export default function Home() {
       const pageNumber = `page${index}`;
       pages[pageNumber] = [];
     }
-    console.log("EL HACER OBJETO");
   };
 
-  const dividirArray = () => {
-    const myNewArray = myArray.map((e) => e);
+  const dividirArray = (data = myArray) => {
+    const myNewArray = data.map((e) => e);
     let paginaActual = 1;
-    for (let id = 0; id <= myNewArray.length; id++) {
+    for (let id = 0; id <= data.length; id++) {
       let pageMaker = `page${paginaActual.toString()}`;
       if (id % limit === 0) {
         let savePage = myNewArray.slice(
@@ -110,12 +118,60 @@ export default function Home() {
   //pagination
   const handleOnPageChange = (data) => {
     let selected = data.selected + 1;
+    console.log("HANDLE CHANGE BY LOCATION");
     setPage(selected);
   };
-
   //select
   const handleChangeLocation = (selectedOption) => {
     setSelectedLocation(selectedOption);
+    // cortarIds(selectedOption);
+  };
+
+  const cortarIds = () => {
+    let resultsLocationIds = [];
+    setTotalCharacters(selectedLocation.residents.length);
+    console.log("ENTRA AL CORTAR");
+    selectedLocation.residents.map((e) => {
+      if (e.length === 45) {
+        resultsLocationIds.push(e.substr(-3));
+      } else if (e.length === 44) {
+        resultsLocationIds.push(e.substr(-2));
+      } else if (e.length === 43) {
+        resultsLocationIds.push(e.substr(-1));
+      }
+    });
+    dividirArrayLocation(resultsLocationIds);
+    pageDataByLocation();
+    fetchCaractersByLocation();
+  };
+
+  const dividirArrayLocation = (resultsLocationIds) => {
+    console.log("PAGINACION DE LOCATION");
+    let nuevoArray = resultsLocationIds.map((e) => e);
+    let paginaActual = 1;
+    for (let id = 0; id <= nuevoArray.length; id++) {
+      let pageMaker = `page${paginaActual.toString()}`;
+      if (id % limit === 0) {
+        let savePage = nuevoArray.slice(
+          (paginaActual - 1) * limit,
+          paginaActual * limit
+        );
+        pages[pageMaker] = savePage;
+        paginaActual++;
+      }
+    }
+  };
+  const pageDataByLocation = () => {
+    let pageName = `page${page}`;
+    let data = pages[pageName].join();
+    setActualPageIds(data);
+    // fetchCaractersByLocation();
+  };
+  const fetchCaractersByLocation = async () => {
+    console.log("ENTRA AL FETCH");
+    const charactersData = await getLimitCharacters(actualPageIds);
+    setCharacters(charactersData);
+    setIsLoading(false);
   };
   return (
     <Container>
@@ -128,31 +184,37 @@ export default function Home() {
         options={options}
       />
       <h1>Personajes</h1>
-      <ListContainer>
-        {!characters.length >= 1 ? (
-          <CharacterCard characterData={characters} />
-        ) : (
-          characters.map((item) => {
-            return <CharacterCard key={item.id} characterData={item} />;
-          })
-        )}
-      </ListContainer>
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        <ReactPaginate
-          className="paginate"
-          previousLabel={"<"}
-          nextLabel={">"}
-          breakLabel={<a href="">...</a>}
-          breakClassName={"break-me"}
-          pageCount={totalPaginas}
-          marginPagesDisplayed={1}
-          pageRangeDisplayed={5}
-          onPageChange={handleOnPageChange}
-          containerClassName={"pagination"}
-          subContainerClassName={"pages pagination"}
-          activeClassName={"active"}
-        />
-      </div>
+      {isLoading ? (
+        <h1>Cargando</h1>
+      ) : (
+        <div>
+          <ListContainer>
+            {!characters.length >= 1 ? (
+              <CharacterCard characterData={characters} />
+            ) : (
+              characters.map((item) => {
+                return <CharacterCard key={item.id} characterData={item} />;
+              })
+            )}
+          </ListContainer>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <ReactPaginate
+              className="paginate"
+              previousLabel={"<"}
+              nextLabel={">"}
+              breakLabel={<a href="">...</a>}
+              breakClassName={"break-me"}
+              pageCount={totalPaginas}
+              marginPagesDisplayed={1}
+              pageRangeDisplayed={5}
+              onPageChange={handleOnPageChange}
+              containerClassName={"pagination"}
+              subContainerClassName={"pages pagination"}
+              activeClassName={"active"}
+            />
+          </div>
+        </div>
+      )}
     </Container>
   );
 }
