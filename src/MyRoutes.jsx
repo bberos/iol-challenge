b8c34ed583fd0857d3f0b256ce839e726e4afe43
bberos <bberos.171@gmail.com>
2022-01-18 13:50:19 -0300

    feat(Routes): favourites

--- src/MyRoutes.jsx
+++ src/MyRoutes.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import CharacterDetail from "./screens/CharacterDetail";
+import Favourites from "./screens/Favourites";
 import Home from "./screens/Home";
 function MyRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path={"/character/:id"} element={<CharacterDetail />} />
+      <Route path={"/favourites"} element={<Favourites />} />
     </Routes>
   );
 }
