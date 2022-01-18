7d126ff4b8f6bc6ec1faaf676b5898d4770676b0
bberos <bberos.171@gmail.com>
2022-01-18 17:15:14 -0300

    styles(Home): remove pagination styles

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -3,17 +3,6 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  .paginate {
-    display: flex;
-    width: 100%;
-    justify-content: center;
-    li {
-      list-style: none;
-      cursor: pointer;
-      padding: 3px 3px;
-      border: 1px solid grey;
-    }
-  }
 `;
 export const ListContainer = styled.div`
   display: flex;
