5b33f24cc389482018b048c952c10cd26bd94bd5
bberos <bberos.171@gmail.com>
2022-01-18 01:47:30 -0300

    styles(Home): paginate styles

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -3,6 +3,17 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  .paginate {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    li {
+      list-style: none;
+      cursor: pointer;
+      padding: 3px 3px;
+      border: 1px solid grey;
+    }
+  }
 `;
 export const ListContainer = styled.div`
   display: flex;
