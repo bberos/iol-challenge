2e0152c6437a2b2ec46b6497f2d1c63dbeb8293b
bberos <bberos.171@gmail.com>
2022-01-19 02:50:31 -0300

    ref(HomeScreen): refactor

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,67 +1,37 @@
-import React, { useEffect, useState } from "react";
-import CharacterCard from "../../components/CharacterCard";
-import { Container, ListContainer } from "./styles";
-import ReactSelect from "react-select";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Paginate from "../../components/Paginate";
 
-//hooks
+import { Container, SubHeader } from "./styles";
+import Paginate from "../../components/Paginate";
 import usePagination from "../../hooks/usePagination";
+import SelectLocation from "../../components/SelectLocation";
+import Characters from "../../components/Characters";
 
 export default function Home() {
   const [selectedLocation, setSelectedLocation] = useState();
-  const [page, setPage] = useState(1);
-  //hooks
-  const { isLoading, characters, totalPaginas, options } = usePagination({
-    limit: 15,
-    selectedLocation,
-    page,
-  });
-  //select
-  const handleChangeLocation = (selectedOption) => {
-    setSelectedLocation(selectedOption);
-    setPage(1);
-  };
-  //pagination
-  const handleOnPageChange = (data) => {
-    let selected = data.selected + 1;
-    setPage(selected);
-  };
+  //Custom hook
+  const { isLoading, characters, totalPaginas, options, page, setPage } =
+    usePagination({
+      limit: 15,
+      selectedLocation,
+    });
 
   return (
     <Container>
-      <h2>Buscar por locacion</h2>
-      <ReactSelect
-        value={selectedLocation}
-        onChange={handleChangeLocation}
-        // clearable={this.state.clearable}
-        // searchable={this.state.searchable}
-        options={options}
-      />
-      <Link to="/favourites">
-        <button>Favoritos</button>
-      </Link>
+      <SubHeader>
+        <SelectLocation
+          value={selectedLocation}
+          options={options}
+          setSelectedLocation={setSelectedLocation}
+          setPage={setPage}
+        />
+        <Link to="/favourites">
+          <h1>Ver Favoritos</h1>
+        </Link>
+      </SubHeader>
       <h1>Personajes</h1>
-      {isLoading ? (
-        <h1>Cargando</h1>
-      ) : (
-        <div>
-          <ListContainer>
-            {!characters.length >= 1 ? (
-              <CharacterCard characterData={characters} />
-            ) : (
-              characters.map((item) => {
-                return <CharacterCard key={item.id} characterData={item} />;
-              })
-            )}
-          </ListContainer>
-          <Paginate
-            totalPaginas={totalPaginas}
-            page={page}
-            handleOnPageChange={handleOnPageChange}
-          />
-        </div>
-      )}
+      {isLoading ? <h1>Cargando</h1> : <Characters characters={characters} />}
+      <Paginate totalPaginas={totalPaginas} page={page} setPage={setPage} />
     </Container>
   );
 }
