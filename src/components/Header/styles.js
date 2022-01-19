7f5b8bc58639c01b71475746db6f3a52a9b2acfb
bberos <bberos.171@gmail.com>
2022-01-19 02:47:15 -0300

    styles(Header)

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,7 +1,17 @@
 import styled from "styled-components";
-
 export const Container = styled.div`
   display: flex;
   justify-content: center;
   padding: 5px 25px;
+  align-items: center;
+  position: sticky;
+  z-index: 2;
+  top: 0;
+  background-color: #030303c2;
+  box-shadow: 1px 4px 7px 0px rgba(0, 0, 0, 0.75);
+  -webkit-box-shadow: 1px 4px 7px 0px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 1px 4px 7px 0px rgba(0, 0, 0, 0.75);
+  img {
+    width: 250px;
+  }
 `;
