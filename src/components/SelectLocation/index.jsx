e3b2820cec5145c791fec2455646ced48398624d
bberos <bberos.171@gmail.com>
2022-01-19 02:48:39 -0300

    feat(SelectLocation) new component

---
+++ src/components/SelectLocation/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactSelect from "react-select";
+import { Container } from "./styles";
+export default function SelectLocation(props) {
+  const { options, value, setSelectedLocation, setPage } = props;
+  const handleChangeLocation = (selectedOption) => {
+    setSelectedLocation(selectedOption);
+    setPage(1);
+  };
+  return (
+    <Container>
+      <ReactSelect
+        value={value}
+        onChange={handleChangeLocation}
+        options={options}
+        placeholder="Buscar por Locación"
+        className="select"
+      />
+    </Container>
+  );
+}
