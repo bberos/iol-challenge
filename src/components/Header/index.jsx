a4822a075b30be37115052d8fa2fa788a7ca280f
bberos <bberos.171@gmail.com>
2022-01-19 02:47:05 -0300

    ref(Header)

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Logo from "./../../assets/images/rickandmorty_logo.svg";
 import { Container } from "./styles";
 function Header() {
   return (
     <Container>
-      <img src={Logo} alt="Logo" />
+      <Link to="/">
+        <img src={Logo} alt="Logo" />
+      </Link>
     </Container>
   );
 }
