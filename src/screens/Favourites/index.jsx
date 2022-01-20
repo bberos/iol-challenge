c8c0a14c4118aab64058f7372a7429f1b4b65957
bberos <bberos.171@gmail.com>
2022-01-20 05:01:21 -0300

    feat(Favourites): Loader

--- src/screens/Favourites/index.jsx
+++ src/screens/Favourites/index.jsx
@@ -4,6 +4,7 @@ import Characters from "../../components/Characters";
 import Paginate from "../../components/Paginate";
 import { useFavourites } from "../../context/FavouritesProvider";
 import usePagination from "../../hooks/usePagination";
+import Loader from "../../components/Loader";
 
 export default function Favourites() {
   const { favCharactersIds, setIsFavouritePage } = useFavourites();
@@ -19,12 +20,17 @@ export default function Favourites() {
   return (
     <Container>
       <h1 className="title">Mis Favoritos</h1>
-      {isLoading || characters.info ? (
-        <h1>Cargando</h1>
+      {/* {isLoading || characters.info ? ( */}
+      {characters.info ? (
+        <>
+          <Loader />
+        </>
       ) : (
-        <Characters characters={characters} />
+        <>
+          <Characters characters={characters} />
+          <Paginate totalPaginas={totalPaginas} page={page} setPage={setPage} />
+        </>
       )}
-      <Paginate totalPaginas={totalPaginas} page={page} setPage={setPage} />
     </Container>
   );
 }
