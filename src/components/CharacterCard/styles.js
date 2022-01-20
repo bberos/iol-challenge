3a8c7d736e5384e5c10e96b4faa5f94fb0d0888f
bberos <bberos.171@gmail.com>
2022-01-19 23:32:11 -0300

    styles(CharacterCard): desktop & laptop styles

--- src/components/CharacterCard/styles.js
+++ src/components/CharacterCard/styles.js
@@ -1,26 +1,58 @@
 import styled from "styled-components";
+import { media } from "./../../styles/media";
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  background-color: #121212;
   width: 15vw;
   align-items: center;
-  border-radius: 5px;
   margin: 10px 10px;
-  cursor: pointer;
+  /* cursor: pointer; */
   transition: all 0.5s;
   z-index: 1;
   &:hover {
     opacity: 0.8;
   }
-  h1 {
-    font-size: 0.7rem;
-    width: 100%;
-    padding: 5px 0px;
-  }
   .characterImage {
     width: 100%;
     border-radius: 0 0 5px 5px;
+  }
+`;
+export const Head = styled.div`
+  background-color: #121212;
+  height: 5vh;
+  width: 100%;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  display: flex;
+  padding: 5px 0px;
+  justify-content: center;
+  ${media.laptop} {
+    height: 6.5vh;
+  }
+`;
+export const HeadContainer = styled.div`
+  display: flex;
+  width: 100%;
+  .nameContainer {
+    width: 85%;
+    ${media.laptop} {
+      width: 83%;
+    }
+  }
+  h1 {
+    font-size: 0.67rem;
+    width: 100%;
+    padding: 5px 0px;
+    text-align: left;
+    padding-left: 7px;
+  }
+  .icon {
+    z-index: 10;
+    cursor: pointer;
+    transition: all 0.2s;
+    &:hover {
+      transform: scale(1.05);
+    }
   }
 `;
