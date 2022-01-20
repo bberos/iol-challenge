3db9cf31be6df9d190c957d53389c75554703090
bberos <bberos.171@gmail.com>
2022-01-19 23:37:28 -0300

    feat(HomeScreen): setIsFavourite useFavouriteContext

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,20 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-import { Container, SubHeader } from "./styles";
+import { Container, SubHeader, FavouriteButton } from "./styles";
 import Paginate from "../../components/Paginate";
 import usePagination from "../../hooks/usePagination";
 import SelectLocation from "../../components/SelectLocation";
 import Characters from "../../components/Characters";
+import { ReactComponent as FavIcon } from "./../../assets/icons/pickleFav.svg";
+import { useFavourites } from "../../context/FavouritesProvider";
 
 export default function Home() {
   const [selectedLocation, setSelectedLocation] = useState();
+
   //Custom hook
   const { isLoading, characters, totalPaginas, options, page, setPage } =
     usePagination({
       limit: 15,
       selectedLocation,
     });
+  const { setIsFavouritePage } = useFavourites();
+
+  useEffect(() => {
+    setIsFavouritePage(false);
+  }, []);
 
   return (
     <Container>
@@ -26,10 +34,13 @@ export default function Home() {
           setPage={setPage}
         />
         <Link to="/favourites">
-          <h1>Ver Favoritos</h1>
+          <FavouriteButton>
+            <h1>Ver Favoritos</h1>
+            <FavIcon width="25px" height="30px" fill="#06750e" />
+          </FavouriteButton>
         </Link>
       </SubHeader>
-      <h1>Personajes</h1>
+      <h1 className="title">Personajes</h1>
       {isLoading ? <h1>Cargando</h1> : <Characters characters={characters} />}
       <Paginate totalPaginas={totalPaginas} page={page} setPage={setPage} />
     </Container>
