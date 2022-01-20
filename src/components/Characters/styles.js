ac4d3d43d24ca083e4ecbc5237e9cc5f17783018
bberos <bberos.171@gmail.com>
2022-01-19 23:32:22 -0300

    styles(Character): desktop & laptop styles

--- src/components/Characters/styles.js
+++ src/components/Characters/styles.js
@@ -1,7 +1,16 @@
 import styled from "styled-components";
+import { media } from "./../../styles/media";
+
 export const Container = styled.div``;
 export const ListContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
+  padding: 0px 10px;
+  ${media.laptopL} {
+    padding: 0px 60px;
+  }
+  ${media.desktopL} {
+    padding: 0px 180px;
+  }
 `;
