67758cabbac3ae3a219806f33332f1a068980ced
bberos <bberos.171@gmail.com>
2022-01-20 05:00:02 -0300

    feat(Loader)

---
+++ src/components/Loader.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Lottie from "react-lottie";
+import animationData from "./../assets/loader.json";
+export default function Loader() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <>
+      <Lottie options={defaultOptions} height={200} width={200} />
+    </>
+  );
+}
