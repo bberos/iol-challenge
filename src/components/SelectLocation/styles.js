42f33a6fe904f77745324cac096a0564f57add44
bberos <bberos.171@gmail.com>
2022-01-19 23:34:00 -0300

    styles(SelectLocation): desktop & laptop styles

--- src/components/SelectLocation/styles.js
+++ src/components/SelectLocation/styles.js
@@ -1,5 +1,55 @@
 import styled from "styled-components";
 export const Container = styled.div`
-  background-color: "red";
-  width: 200px;
+  width: 250px;
+  margin-right: 100px;
+  .select {
+    color: #fdfdfd;
+    box-shadow: 1px 7px 12px 0px rgb(0 0 0 / 75%);
+    -webkit-box-shadow: 1px 7px 12px 0px rgb(0 0 0 / 75%);
+    -moz-box-shadow: 1px 7px 12px 0px rgba(0, 0, 0, 0.75);
+    transition: all 0.2s;
+    &:hover {
+      transform: scale(1.02);
+    }
+  }
+  .selectPrefix {
+    &__placeholder {
+      color: #fdfdfd;
+    }
+    &__single-value {
+      color: #fdfdfd;
+    }
+    &__control {
+      background-color: #282c34;
+      border-color: hsl(72, 71%, 56%, 0.8);
+      box-shadow: none;
+      &:hover {
+        border-color: hsl(72, 71%, 56%, 0.8);
+      }
+    }
+    &__menu {
+      background-color: #282c34;
+    }
+    &__option {
+      &:hover {
+        background-color: #40b5cbb3;
+      }
+      &--is-focused {
+        background-color: #40b5cbb3;
+      }
+    }
+    &__indicator-separator {
+      background-color: #fdfdfd;
+      &:hover {
+        background-color: #fdfdfd;
+        opacity: 1;
+      }
+    }
+    &__dropdown-indicator {
+      color: #fdfdfd;
+      &:hover {
+        color: #bfde3fcc;
+      }
+    }
+  }
 `;
