809cf9fbbc64d7f13c97975bb7c294092cb8bfe7
bberos <bberos.171@gmail.com>
2022-01-19 23:33:04 -0300

    styles(Header): backIcon styles

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -11,7 +11,18 @@ export const Container = styled.div`
   box-shadow: 1px 4px 7px 0px rgba(0, 0, 0, 0.75);
   -webkit-box-shadow: 1px 4px 7px 0px rgba(0, 0, 0, 0.75);
   -moz-box-shadow: 1px 4px 7px 0px rgba(0, 0, 0, 0.75);
-  img {
+  .backIcon {
+    position: absolute;
+    left: 0;
+    margin-left: 20px;
+    top: 40px;
+    width: 60px;
+    transition: all 0.2s;
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
+  .logo {
     width: 250px;
   }
 `;
