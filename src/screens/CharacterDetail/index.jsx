17ce5cc43b5c51671443b4130e5e3fb5e91ec6ac
bberos <bberos.171@gmail.com>
2022-01-20 01:39:19 -0300

    feat(ChDetail): layout & useFavourite

--- src/screens/CharacterDetail/index.jsx
+++ src/screens/CharacterDetail/index.jsx
@@ -7,10 +7,12 @@ import {
   ImageContainer,
   InfoContainer,
 } from "./styles";
+import { useFavourites } from "../../context/FavouritesProvider";
 export default function CharacterDetail(props) {
   const [isLoading, setIsLoading] = useState(true);
   const [character, setCharacter] = useState();
-  const navigate = useNavigate();
+  const { setIsFavouritePage } = useFavourites();
+
   const chId = useParams();
   const fetchCharacter = async () => {
     const characterData = await getCharacter(chId.id);
@@ -19,24 +21,48 @@ export default function CharacterDetail(props) {
   };
   useEffect(() => {
     fetchCharacter();
+    setIsFavouritePage(true);
   }, []);
+
   return (
     <div>
       {isLoading ? (
         <h1>Cargando</h1>
       ) : (
         <Container>
-          <CardContainer>
+          <CardContainer
+            style={{
+              borderColor:
+                character.status === "Dead"
+                  ? "#b73939"
+                  : "hsl(72, 71%, 56%, 0.8)",
+            }}
+          >
             <ImageContainer>
-              <img className="image" src={character.image} alt="photo" />
+              <img
+                className="image"
+                src={character.image}
+                alt="photo"
+                style={{
+                  borderRadius: "20px",
+                  filter: character.status === "Dead" ? "grayscale(1)" : "none",
+                }}
+              />
             </ImageContainer>
             <InfoContainer>
-              <h1>Nombre:{character.name} </h1>
-              <h1>Estado:{character.status} </h1>
-              <h1>Origen:{character.origin.name} </h1>
-              <h1>Locacion:{character.location.name} </h1>
+              <h1>
+                <b>Nombre</b>: {character.name}
+              </h1>
+              <h1>
+                <b>Origen</b>: {character.origin.name}
+              </h1>
+              <h1>
+                <b>Locacion</b>: {character.location.name}
+              </h1>
+              <h1>
+                <b>Estado</b>: {character.status}
+              </h1>
             </InfoContainer>
-            <button onClick={() => navigate(-1)}>Volver</button>
           </CardContainer>
         </Container>
       )}
