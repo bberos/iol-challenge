70136365fe69cd1e8bbb29b333b8873a5b371306
bberos <bberos.171@gmail.com>
2022-01-19 23:37:11 -0300

    feat(FavouritesScreen): setIsFavourite useFavouriteContext

--- src/screens/Favourites/index.jsx
+++ src/screens/Favourites/index.jsx
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Characters from "../../components/Characters";
 import Paginate from "../../components/Paginate";
 import { useFavourites } from "../../context/FavouritesProvider";
 import usePagination from "../../hooks/usePagination";
 
 export default function Favourites() {
-  const { favCharactersIds } = useFavourites();
+  const { favCharactersIds, setIsFavouritePage } = useFavourites();
   const { page, setPage, isLoading, totalPaginas, characters } = usePagination({
     limit: 15,
     isFavouritePage: true,
     totalFavChars: favCharactersIds.length,
     favCharactersIds,
   });
-
+  useEffect(() => {
+    setIsFavouritePage(true);
+  });
   return (
     <div>
       <h1>Mis Favoritos</h1>
-      {isLoading ? <h1>Cargando</h1> : <Characters characters={characters} />}
+      {isLoading || characters.info ? (
+        <h1>Cargando</h1>
+      ) : (
+        <Characters characters={characters} />
+      )}
       <Paginate totalPaginas={totalPaginas} page={page} setPage={setPage} />
     </div>
   );
