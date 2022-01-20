555d1e3292ff5a051221ce0af3c97db02ef2d22d
bberos <bberos.171@gmail.com>
2022-01-19 23:32:45 -0300

    feat(Header): render back icon if is favourite page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useFavourites } from "../../context/FavouritesProvider";
 import Logo from "./../../assets/images/rickandmorty_logo.svg";
+import BackIcon from "./../../assets/icons/backIcon.svg";
 import { Container } from "./styles";
 function Header() {
+  const { isFavouritePage } = useFavourites();
+
   return (
     <Container>
+      {isFavouritePage ? (
+        <Link to="/">
+          <img src={BackIcon} alt="Back" className="backIcon" />
+        </Link>
+      ) : null}
       <Link to="/">
-        <img src={Logo} alt="Logo" />
+        <img className="logo" src={Logo} alt="Logo" />
       </Link>
     </Container>
   );
