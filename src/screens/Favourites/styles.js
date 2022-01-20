3af89b814a02e62feb11a5c3186db88004c149e3
bberos <bberos.171@gmail.com>
2022-01-20 01:40:17 -0300

    styles(FavouritesScreen): desktop & laptop queries

---
+++ src/screens/Favourites/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  .title {
+    text-transform: uppercase;
+    color: #40b5cb;
+    font-size: 2rem;
+    font-weight: 900;
+    -webkit-text-stroke-width: 0.01px;
+    -webkit-text-stroke-color: #b0cf40b3;
+  }
+`;
