99163f8d7c47364205f15a01538913cf8c116e89
bberos <bberos.171@gmail.com>
2022-01-20 01:38:19 -0300

    feat(Header): add back btn if isFav || detail

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useFavourites } from "../../context/FavouritesProvider";
 import Logo from "./../../assets/images/rickandmorty_logo.svg";
 import BackIcon from "./../../assets/icons/backIcon.svg";
 import { Container } from "./styles";
 function Header() {
   const { isFavouritePage } = useFavourites();
-
+  const navigate = useNavigate();
   return (
     <Container>
       {isFavouritePage ? (
-        <Link to="/">
-          <img src={BackIcon} alt="Back" className="backIcon" />
-        </Link>
+        <img
+          src={BackIcon}
+          alt="Back"
+          className="backIcon"
+          onClick={() => navigate(-1)}
+        />
       ) : null}
       <Link to="/">
         <img className="logo" src={Logo} alt="Logo" />
