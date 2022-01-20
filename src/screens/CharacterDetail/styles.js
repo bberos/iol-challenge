6afec5e62f4bcec166db005e159daa16d1962efe
bberos <bberos.171@gmail.com>
2022-01-19 23:36:44 -0300

    styles(CharacterDetail)

---
+++ src/screens/CharacterDetail/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const CardContainer = styled.div`
+  background-color: red;
+  width: 60%;
+  display: flex;
+`;
+
+export const ImageContainer = styled.div``;
+
+export const InfoContainer = styled.div`
+  h1 {
+    font-size: 1.5rem;
+    text-align: left;
+  }
+`;
